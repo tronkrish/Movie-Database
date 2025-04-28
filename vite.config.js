05e40777282727c136b2edf64a084c0a82bf1fb2
tronkrish <sriprasanna8585@gmail.com>
2025-04-28 13:26:23 +0530

    Updated File Path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Movie-Database"
+  server: {
+    // This will fix the 404 issue
+    historyApiFallback: true,
+  },
 })
